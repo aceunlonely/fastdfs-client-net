46e69c7d9a3c09228a55a646d396ef0050659466
apporoad <apporoadina@163.com>
2018-06-03 22:49:30 +0800

    add

---
+++ code/fastdfs-client-net/fastdfs-client-net/FDFSConfig.cs
@@ -0,0 +1,44 @@
+﻿using fastdfs_client_net.thirds;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace fastdfs_client_net
+{
+	/// <summary>
+	/// fastDfs外部配置
+	/// </summary>
+	public class FDFSConfig
+	{
+		public static string TrackerNodes { get { return ConfigureHelper.GetConfigureValue("FileServer_Nodes", ""); } }
+
+		public static string DefaultGroupName { get { return ConfigureHelper.GetConfigureValue("FileServer_GroupName", "group1"); } }
+
+		public static bool IsLogicDelete { get { return ConfigureHelper.GetConfigureBoolValue("FileServer_IsLogicDelete", false); } }
+
+		public static string DownloadUrl { get { return ConfigureHelper.GetConfigureValue("FileServer_DownloadUrl", "http://192.168.12.215:8080/group1/"); } }
+
+		public static string ProxyDownloadUrl { get { return ConfigureHelper.GetConfigureValue("FileServer_ProxyDownloadUrl", DownloadUrl); } }
+
+		public static string FastDFSDownloadPath
+		{
+			get
+			{
+				string strPath = ConfigureHelper.GetConfigureValue("FileServer_TempFilePath", Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FileServer_TempFile"));
+				if (!Directory.Exists(strPath))
+				{
+					Directory.CreateDirectory(strPath);
+				}
+				return strPath;
+			}
+		}
+
+		public static int FileServerFailRetryTime { get { return ConfigureHelper.GetConfigureIntValue("FileServer_FailRetryTime", 10); } }
+
+		public static int FileServerUploadFailRetryTime { get { return ConfigureHelper.GetConfigureIntValue("FileServer_UploadFailRetryTime", FileServerFailRetryTime); } }
+
+		public static int FileServerDownloadFailRetryTime { get { return ConfigureHelper.GetConfigureIntValue("FileServer_DownloadFailRetryTime", FileServerFailRetryTime); } }
+	}
+}
---
+++ code/fastdfs-client-net/fastdfs-client-net/thirds/ConfigureHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace fastdfs_client_net.thirds
+{
+	/// <summary>
+	/// 配置帮助 get form https://github.com/hyd-raiders/tickDotNet
+	/// </summary>
+	class ConfigureHelper
+	{
+		/// <summary>
+		/// 获取配置值
+		/// </summary>
+		/// <param name="key">关键词</param>
+		/// <param name="defaultValue">默认值</param>
+		/// <returns></returns>
+		public static string GetConfigureValue(string key, string defaultValue = "")
+		{
+			var value = ConfigurationManager.AppSettings[key];
+			if (string.IsNullOrEmpty(value))
+			{
+				value = defaultValue;
+			}
+			return value;
+		}
+
+		/// <summary>
+		/// 获取配置值
+		/// </summary>
+		/// <param name="key">关键词</param>
+		/// <param name="defaultValue">默认值</param>
+		/// <returns></returns>
+		public static int GetConfigureIntValue(string key, int defaultValue = 0)
+		{
+			var value = ConfigurationManager.AppSettings[key];
+
+			int realValue;
+
+			if (int.TryParse(value, out realValue))
+			{
+				return realValue;
+			}
+			else
+			{
+				return defaultValue;
+			}
+		}
+
+		/// <summary>
+		/// 获取配置值
+		/// </summary>
+		/// <param name="key">关键词</param>
+		/// <param name="defaultValue">默认值</param>
+		/// <returns></returns>
+		public static bool GetConfigureBoolValue(string key, bool defaultValue = false)
+		{
+			var value = ConfigurationManager.AppSettings[key];
+			if (string.IsNullOrEmpty(value))
+			{
+				return defaultValue;
+			}
+			if (defaultValue)
+			{
+				if (value.ToLower() == "false" || value == "0")
+				{
+					return false;
+				}
+				return true;
+			}
+			else
+			{
+				if (value.ToLower() == "true" || value == "1")
+				{
+					return true;
+				}
+				return false;
+			}
+		}
+	}
+}
